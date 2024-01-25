ee7a41ab199fd5c229a95685b1630e20d55dead0
JhiWhoon Kang <kangjw0914@naver.com>
2024-01-26 02:45:17 +0900

    demo

---
+++ RCWS_Situation-room/RCWS_Situation-room(only udp)/RCWS_Situation-room(only udp)/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace RCWS_Situation_room_only_udp_
+{
+    public partial class Form1 : Form
+    {
+        private 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RCWS_Situation-room/RCWS_Situation-room/define.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCWS_Situation_room
+{
+    internal class define
+    {
+        public const string SERVER_IP = "127.0.0.1";
+        //public const string SERVER_IP = "192.168.0.53";
+        public const int TCPPORT = 9001;
+        public const int UDPPORT = 9000;
+
+        public const int MAX_ANGLE = 0;
+        public const int MIN_ANGLE = 0;
+    }
+}
